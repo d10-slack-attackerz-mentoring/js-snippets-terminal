c6654a1cecfc4320a8874efcf628c49d59516477
losrobbos <losrobbos@web.de>
2023-03-31 08:59:04 +0200

    Fixed typo

--- 04-arrays/arrays-of-objects.js
+++ 04-arrays/arrays-of-objects.js
@@ -2,5 +2,5 @@ const arr = [
   { name: "Zoo", seasons: 3 },
   { name: "Anne with an E", seasons: 3 },
   { name: "Vikings Walhalla", seasons: 4 } ,
-  { name: "Soutpark", seasons: 26 }
+  { name: "Southpark", seasons: 26 }
 ]
