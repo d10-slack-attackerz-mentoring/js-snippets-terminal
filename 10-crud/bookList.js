95619ccc6d3e7f7b833f93d798829c94404414e3
losrobbos <losrobbos@web.de>
2023-05-05 12:08:02 +0200

    Polishing

--- 10-crud/bookList.js
+++
@@ -1,8 +0,0 @@
-// searching items
-/// filter => if we allow searching multiple items 
-/// find => if we just search exactly ONE item
-
-
-// deleting von items => filter
-
-// updating an item => map or find
